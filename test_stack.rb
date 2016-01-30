ae1475dd64fb1bcd21db2d49961bb1b867e873f8
HIGAKI Masaru <mas.higa@gmail.com>
2016-01-30 16:52:50 +0900

    step8

--- test_stack.rb
+++ test_stack.rb
@@ -41,4 +41,10 @@ class TestStack < Test::Unit::TestCase
     @stack.pop
     assert_equal 1, @stack.size
   end
+
+  def test_pop_by_3_5_stacked
+    @stack.push 3
+    @stack.push 5
+    assert_equal 5, @stack.pop
+  end
 end
